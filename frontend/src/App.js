ae74cc35b4cf30c6f5fb93e277cd936dead59792
Joseph Aje <ajejoseph22@gmail.com>
2021-03-29 19:54:57 +0100

    chore: added spinner and error pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,41 @@
 import { useEffect, useState } from "react";
 import { getCurrencies } from "./services/currencies";
+import Spinner from "./components/spinner";
 import "./App.css";
+import ErrorPage from "./components/error-page";
 
 function App() {
   const [currencies, setCurrencies] = useState();
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchCurrenciesAndSetState = async () => {
+    try {
+      setIsLoading(true);
+      setCurrencies(await getCurrencies());
+    } catch ({ message }) {
+      setError(message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     (async () => {
-      try {
-        setCurrencies(await getCurrencies());
-      } catch ({ message }) {
-      } finally {
-      }
+      await fetchCurrenciesAndSetState();
     })();
   }, []);
+
+  if (isLoading) return <Spinner color="#000" />;
+
+  if (error)
+    return (
+      <ErrorPage
+        retryFunction={fetchCurrenciesAndSetState}
+        message={error}
+        color="#000"
+      />
+    );
 
   return (
     <div id="app">
---
+++ frontend/src/components/error-page/index.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledErrorPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  height: 85vh;
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+const ErrorPage = ({ message, retryFunction }) => (
+  <StyledErrorPage>
+    <p>{message}</p>
+    <button onClick={retryFunction}>Retry</button>
+  </StyledErrorPage>
+);
+
+export default ErrorPage;
