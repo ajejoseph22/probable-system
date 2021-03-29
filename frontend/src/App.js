7c5543b768f2ee8e44a1a32ffb9eba8887c04d60
Joseph Aje <ajejoseph22@gmail.com>
2021-03-29 21:19:32 +0100

    chore: added toggle functionalities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { getCurrencies } from "./services/currencies";
 import Spinner from "./components/spinner";
-import "./App.css";
 import ErrorPage from "./components/error-page";
+import { Switch } from "antd";
+import "./App.css";
+import "react-toggle/style.css";
 
 function App() {
   const [currencies, setCurrencies] = useState();
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [
+    isDisplayingUsSupportedOnly,
+    setIsDisplayingUsSupportedOnly,
+  ] = useState(false);
+  const [isTestModeOnly, setIsTestModeOnly] = useState(false);
 
   const fetchCurrenciesAndSetState = async () => {
     try {
@@ -18,6 +25,32 @@ function App() {
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const filteredCurrencies = useMemo(() => {
+    let currenciesToFilter = currencies || [];
+
+    if (isDisplayingUsSupportedOnly) {
+      currenciesToFilter = currenciesToFilter.filter(
+        (currency) => currency.isSupportedInUS
+      );
+    }
+
+    if (isTestModeOnly) {
+      currenciesToFilter = currenciesToFilter.filter(
+        (currency) => currency.supportsTestMode
+      );
+    }
+
+    return currenciesToFilter;
+  }, [currencies, isDisplayingUsSupportedOnly, isTestModeOnly]);
+
+  const handleOnChangeUsSupportedOnly = () => {
+    setIsDisplayingUsSupportedOnly((prevState) => !prevState);
+  };
+
+  const handleOnChangeTestModeOnly = () => {
+    setIsTestModeOnly((prevState) => !prevState);
   };
 
   useEffect(() => {
@@ -39,9 +72,20 @@ function App() {
 
   return (
     <div id="app">
+      <Switch
+        checkedChildren="Show only US supported"
+        unCheckedChildren="Include non US supported"
+        onChange={handleOnChangeUsSupportedOnly}
+      />
+      <br />
+      <Switch
+        checkedChildren="Test mode only"
+        unCheckedChildren="Include non test mode"
+        onChange={handleOnChangeTestModeOnly}
+      />
       <header>CURRENCIES</header>
       <div id="currencies">
-        {(currencies || []).map(({ name }) => (
+        {filteredCurrencies.map(({ name }) => (
           <div className="currency">{name}</div>
         ))}
       </div>
