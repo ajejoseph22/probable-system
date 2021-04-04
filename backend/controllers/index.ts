de8bc2cbea753261d3d46dc602b5444e1008e09c
Joseph Aje <ajejoseph22@gmail.com>
2021-04-04 04:11:15 +0100

    chore: cleanup

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
 
-export default (req: Request, res: Response) =>
+export default (req: Request, res: Response) => {
   res.send("Hi, use the '/exchange' route pls :)");
+};
